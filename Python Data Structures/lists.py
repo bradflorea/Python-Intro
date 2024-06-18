d41d2cc6a24baaa316fc9bcea5ef4c98b75cd649
Brad Florea <bradleyaflorea@gmail.com>
2024-06-18 19:24:17 -0400

    yyaaaaaaaaaa

--- Python Data Structures/lists.py
+++ Python Data Structures/lists.py
@@ -1,1 +1,9 @@
-scores = [49,99,100,78,89,90,100,88,87,8]
+vegan_no_nos = ['eggs', 'cheese', 'meat', 'milk','fish', 'butter']
+
+pie_ingredients = ['flour', 'butter', 'eggs', 'apples','sugar']
+
+for food in pie_ingredients:
+    if food in vegan_no_nos:
+        print(f"OH NO, CANNOT EAT {food}! IT'S NOT VEGAN!")
+    else:
+        print(f'YAY, I CAN EAT {food}!')
