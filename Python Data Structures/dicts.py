88c2deae905ec6caedfe34e6f1abb0069b5e2f25
Brad Florea <bradleyaflorea@gmail.com>
2024-06-20 20:24:01 -0400

    chickens

--- Python Data Structures/dicts.py
+++ Python Data Structures/dicts.py
@@ -4,5 +4,12 @@ chicken = {
     "total_egg_count": 12,
     "egg_chart": {
         "M": True,
+        "T": True,
+        "W": True,
+        "TH": True,
+        "F": True,
+        "S": True,
+        "SU": True,
     },
+    'coop_mates': ['Butters', 'Stevie', 'Henry']
 }
